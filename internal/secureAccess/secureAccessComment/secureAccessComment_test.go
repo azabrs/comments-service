f6a0891b441bdecf3b897e08ab4aa126c5b67670
azabrs <Apukha@mail.ru>
2024-06-05 00:02:08 +0300

    add secureAccess unit test

--- internal/secureAccess/secureAccessComment/secureAccessComment_test.go
+++ internal/secureAccess/secureAccessComment/secureAccessComment_test.go
@@ -2,8 +2,12 @@ package secure_access_comment
 
 import (
 	"comments_service/graph/model"
-	"comments_service/internal/storage"
+	"comments_service/internal/storage/mocks"
+	storage "comments_service/internal/storage/mocks"
+	"fmt"
 	"testing"
+
+	"github.com/stretchr/testify/mock"
 )
 
 func TestSecureAccessComment_Authorize(t *testing.T) {
@@ -20,21 +24,30 @@ func TestSecureAccessComment_Authorize(t *testing.T) {
 		want    string
 		wantErr bool
 	}{
-		// TODO: Add test cases.
+		{
+			name: "correct JWTkey", 
+			fields: fields{
+				JWTKey: "secretkey",
+				}, 
+			args: args{
+				login: "login",
+				}, 
+			wantErr: false,
+		},
+
+
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			auth := SecureAccessComment{
 				JWTKey: tt.fields.JWTKey,
 			}
-			got, err := auth.Authorize(tt.args.login)
+			_, err := auth.Authorize(tt.args.login)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("SecureAccessComment.Authorize() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
-			if got != tt.want {
-				t.Errorf("SecureAccessComment.Authorize() = %v, want %v", got, tt.want)
-			}
+
 		})
 	}
 }
@@ -53,14 +66,74 @@ func TestSecureAccessComment_Authentication(t *testing.T) {
 		args    args
 		wantErr bool
 	}{
-		// TODO: Add test cases.
+		{
+			name: "Valid",
+			fields: fields{
+					JWTKey: "secretkey",
+			},
+			args: args{
+				IdentificationData: model.IdentificationData{
+					Login: "sasha",
+					Token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjUzMTY4ODgwMDUsIkxvZ2luIjoic2FzaGEifQ.dgUOGwLbSQPHujNSotwEIChJ79E0bWtRSFkW_w58niU",
+				},
+				stor: *mocks.NewStorage(t),
+			},
+			wantErr: false,
+		},
+
+		{
+			name: "Invalid_login_in_token",
+			fields: fields{
+					JWTKey: "secretkey",
+			},
+			args: args{
+				IdentificationData: model.IdentificationData{
+					Login: "petr",
+					Token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjUzMTY4ODgwMDUsIkxvZ2luIjoic2FzaGEifQ.dgUOGwLbSQPHujNSotwEIChJ79E0bWtRSFkW_w58niU",
+				},
+				stor: *mocks.NewStorage(t),
+			},
+			wantErr: true,
+		},
+
+		{
+			name: "User_already_registred",
+			fields: fields{
+					JWTKey: "secretkey",
+			},
+			args: args{
+				IdentificationData: model.IdentificationData{
+					Login: "petr",
+					Token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjUzMTc1MzQ3ODAsIkxvZ2luIjoicGV0ciJ9.Q_JzcVD62rK3rFIPASdXtjadnRHrozsTUNNccTv7oVw",
+				},
+				stor: *mocks.NewStorage(t),
+			},
+			wantErr: true,
+		},
+
+		{
+			name: "User_already_registred",
+			fields: fields{
+					JWTKey: "secretkey",
+			},
+			args: args{
+				IdentificationData: model.IdentificationData{
+					Login: "petr",
+					Token: "adsdas",
+				},
+				stor: *mocks.NewStorage(t),
+			},
+			wantErr: true,
+		},
 	}
 	for _, tt := range tests {
+		tt.args.stor.On("IsLoginExist", "sasha").Return(nil)
+		tt.args.stor.On("IsLoginExist", mock.AnythingOfType("string")).Return(fmt.Errorf("error"))
 		t.Run(tt.name, func(t *testing.T) {
 			auth := SecureAccessComment{
 				JWTKey: tt.fields.JWTKey,
 			}
-			if err := auth.Authentication(tt.args.IdentificationData, tt.args.stor); (err != nil) != tt.wantErr {
+			if err := auth.Authentication(tt.args.IdentificationData, &tt.args.stor); (err != nil) != tt.wantErr {
 				t.Errorf("SecureAccessComment.Authentication() error = %v, wantErr %v", err, tt.wantErr)
 			}
 		})
